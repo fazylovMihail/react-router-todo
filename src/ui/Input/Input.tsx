3409f9eed803e6ed84087728bf9a650e0c96af40
fazylovMihail <fazylovmihail726@gmail.com>
2026-04-21 17:41:03 +0500

    Оптимизация

--- src/ui/Input/Input.tsx
+++ src/ui/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, type InputHTMLAttributes } from "react";
+import { forwardRef, memo, type InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   labelText?: string;
@@ -6,23 +6,25 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   error?: string;
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ labelText, modificators = [], error, id, ...props }, ref) => {
-    const className = [
-      "custom-input",
-      ...modificators.map((mod) => `custom-input--${mod}`),
-    ].join(" ");
+export const Input = memo(
+  forwardRef<HTMLInputElement, InputProps>(
+    ({ labelText, modificators = [], error, id, ...props }, ref) => {
+      const className = [
+        "custom-input",
+        ...modificators.map((mod) => `custom-input--${mod}`),
+      ].join(" ");
 
-    return (
-      <div className={className}>
-        <input ref={ref} className="custom-input__field" id={id} {...props} />
-        <label htmlFor={id} className="custom-input__label">
-          {labelText}
-        </label>
-        {error && <span className="custom-input__error">{error}</span>}
-      </div>
-    );
-  },
+      return (
+        <div className={className}>
+          <input ref={ref} className="custom-input__field" id={id} {...props} />
+          <label htmlFor={id} className="custom-input__label">
+            {labelText}
+          </label>
+          {error && <span className="custom-input__error">{error}</span>}
+        </div>
+      );
+    },
+  ),
 );
 
 Input.displayName = "Input";
--- src/ui/NoteCard/NoteCard.tsx
+++ src/ui/NoteCard/NoteCard.tsx
@@ -1,4 +1,4 @@
-import type { FC } from "react";
+import { memo, type FC } from "react";
 import { Button } from "../Button";
 import type { Note } from "../../api";
 
@@ -6,16 +6,18 @@ interface NoteCardProps extends Note {
   onDelete: (id: string) => void;
 }
 
-export const NoteCard: FC<NoteCardProps> = ({ id, title, desc, onDelete }) => {
-  return (
-    <div className="note-card">
-      <h3 className="note-card__title">{title}</h3>
-      {desc && <p className="note-card__desc">{desc}</p>}
-      <Button type="button" onClick={() => onDelete(id)}>
-        Удалить
-      </Button>
-    </div>
-  );
-};
+export const NoteCard: FC<NoteCardProps> = memo(
+  ({ id, title, desc, onDelete }) => {
+    return (
+      <div className="note-card">
+        <h3 className="note-card__title">{title}</h3>
+        {desc && <p className="note-card__desc">{desc}</p>}
+        <Button type="button" onClick={() => onDelete(id)}>
+          Удалить
+        </Button>
+      </div>
+    );
+  },
+);
 
 NoteCard.displayName = "NotesCard";
